cff715de319c47977d3801feb7716dcf4ca7d993
Jonas <jonasexplore@gmail.com>
2022-03-05 18:36:46 -0300

    feat(pages): add home layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,7 @@
 import type { NextPage } from 'next'
-import styles from '../styles/Home.module.scss'
 
-const Home: NextPage = () => {
-  return (
-    <div className={styles.container}>
-    </div>
-  )
-}
+import { HomeLayout } from 'layouts/home'
+
+const Home: NextPage = () => <HomeLayout />
 
 export default Home

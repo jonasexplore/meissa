8715996004f0fe507775992ea99037c98bcde710
Jonas <jonasexplore@gmail.com>
2022-03-06 11:18:25 -0300

    refactor(component): change to favorite button

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,12 +1,10 @@
 import { useState } from "react";
-import { HeartFilled, HeartOutlined } from "@ant-design/icons";
-import { Button, Card, Image, Tag, Typography } from "antd";
-import { Choose } from "react-extras";
+import { Card, Image, Typography } from "antd";
 
-import { DefaultButton } from "components/button";
+import { DefaultButton, FavoriteButton } from "components/buttons";
+import { TagList } from "components/tagList";
 
 import styles from './card.module.scss';
-import { TagList } from "components/tagList";
 
 const { Title, Paragraph } = Typography
 
@@ -26,19 +24,8 @@ const DefaultCard = ({ id, title, types, imageUrl, isFavorite }: CardProps) => {
   }
 
   return (
-    <Card
-      hoverable
-      style={{ width: 240 }}
-      className={styles.card}
-    >
-      <Choose>
-        <Choose.When condition={isFav}>
-          <HeartFilled className={styles[`heart--filled`]} onClick={handleFavorite} />
-        </Choose.When>
-        <Choose.Otherwise>
-          <HeartOutlined className={styles.heart} onClick={handleFavorite} />
-        </Choose.Otherwise>
-      </Choose>
+    <Card hoverable className={styles.card}>
+      <FavoriteButton isFavorite={isFav} onClick={handleFavorite} />
 
       <div className={styles.image}>
         <Image preview={false} alt={title} src={imageUrl} />

25bc550648bf8b20bed68ec9e5bed6bc91a9056f
Jonas <jonasexplore@gmail.com>
2022-03-13 15:40:32 -0300

    chore(pages): added pokemon provider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
 import type { NextPage } from 'next'
 
 import { HomeLayout } from 'layouts/home'
+import { PokemonProvider } from 'contexts'
 
-const Home: NextPage = () => <HomeLayout />
+const Home: NextPage = () =>
+  <PokemonProvider>
+    <HomeLayout />
+  </PokemonProvider>
 
 export default Home

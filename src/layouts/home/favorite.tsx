4e59663fa47a87610c0c6e71b747c79adaafe687
Jonas <jonasexplore@gmail.com>
2022-03-13 15:40:15 -0300

    chore(layout): adapted with context

--- src/layouts/home/favorite.tsx
+++ src/layouts/home/favorite.tsx
@@ -3,13 +3,17 @@ import { CardList } from "components/cardList"
 
 type Props = {
   pokemons: any[]
+  isLoading?: boolean
 }
 
-const FavoriteLayout = ({ pokemons }: Props) => {
+const FavoriteLayout = ({ pokemons, isLoading = false }: Props) => {
+  const title = pokemons.length === 0 ? "Nenhum Pokemon encontrado" :
+    `Olá, você tem ${pokemons.length} pokémon salvo!`
+
   return (
     <>
-      <Typography.Title level={2}>Olá, você tem 01 pokémon salvo!</Typography.Title>
-      <CardList cards={pokemons} />
+      <Typography.Title level={2}>{title}</Typography.Title>
+      <CardList cards={pokemons} isLoading={isLoading} />
     </>
   )
 }
--- src/layouts/home/index.tsx
+++ src/layouts/home/index.tsx
@@ -1,76 +1,30 @@
-import { useState } from "react"
+import { useContext, useState } from "react"
 import { Layout } from "antd"
 import { Choose } from "react-extras"
 
-import { menuItems } from "domain/types"
+import { menuItems } from "models/enums"
 import { Container } from "components/container"
 import { Header } from "components/header"
 import { SearchLayout } from "./search"
-import { AllLayout } from "./all"
+import { CategoryLayout } from "./category"
 import { FavoriteLayout } from "./favorite"
-
-const pokemons = [
-  {
-    id: 1,
-    imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/1.png",
-    isFavorite: false,
-    title: "Bulbasaur",
-    types: ["Grass", "Poison"],
-  },
-  {
-    id: 2,
-    imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/2.png",
-    isFavorite: false,
-    title: "Ivysaur",
-    types: ["Grass", "Poison"],
-  },
-  {
-    id: 3,
-    imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/3.png",
-    isFavorite: false,
-    title: "Venusaur",
-    types: ["Grass", "Poison"],
-  },
-  {
-    id: 4,
-    imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/4.png",
-    isFavorite: false,
-    title: "Charmander",
-    types: ["Fire"],
-  },
-  {
-    id: 5,
-    imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/5.png",
-    isFavorite: false,
-    title: "Charmeleon",
-    types: ["Fire"],
-  },
-  {
-    id: 6,
-    imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/6.png",
-    isFavorite: false,
-    title: "Charizard",
-    types: ["Fire", "Flying"],
-  },
-  {
-    id: 7,
-    imageUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/7.png",
-    isFavorite: false,
-    title: "Squirtle",
-    types: ["Water"],
-  }
-]
+import { PokemonContext } from "contexts"
 
 const HomeLayout = () => {
+  const { pokemons, isLoading } = useContext(PokemonContext)
   const [currentTab, setCurrentTab] = useState<menuItems>(menuItems.favorites);
   const [isSearching, setIsSearching] = useState<boolean>(false);
   const [filtered, setFiltered] = useState<any[]>(pokemons);
   const [filteredAll, setFilteredAll] = useState<any[]>(pokemons);
 
+  const defaultTag = 'Todos'
+
+  const favoritePokemons = pokemons.filter(pokemon => pokemon.isFavorite)
+
   const onSearch = (value: string) => {
     setIsSearching(true)
 
-    const search = pokemons.filter(pokemon => pokemon.title.includes(value))
+    const search = pokemons.filter(pokemon => pokemon.name.includes(value))
     setFiltered(search)
 
     setIsSearching(false)
@@ -79,33 +33,40 @@ const HomeLayout = () => {
   const onSearchByTag = (tag: string) => {
     setIsSearching(true)
 
-    if (tag === 'Todos') {
+    if (tag === defaultTag) {
       setFilteredAll(pokemons)
       return
     }
 
-    const search = pokemons.filter(pokemon => pokemon.types.includes(tag))
+    const search = pokemons.filter(
+      pokemon => pokemon.types?.map(types => types.type.name).includes(tag)
+    )
+
     setFilteredAll(search)
 
     setIsSearching(false)
   }
 
   const tagList = pokemons
-    .map(pokemon => pokemon.types.map(type => type))
+    .map(pokemon => pokemon.types?.map(types => types.type.name))
     .flat()
     .filter((value, index, self) => self.indexOf(value) === index)
 
-  tagList.unshift("Todos")
+  tagList.unshift(defaultTag)
 
   return (
     <div>
-      <Header currentTab={currentTab} setCurrentTab={setCurrentTab} />
+      <Header
+        currentTab={currentTab}
+        setCurrentTab={setCurrentTab}
+        numberOfFavorites={favoritePokemons.length}
+      />
       <Layout style={{ width: '100%' }}>
         <Container>
           <Choose>
             <Choose.When condition={currentTab === menuItems.favorites}>
               <div style={{ padding: 16, height: '100vh' }}>
-                <FavoriteLayout pokemons={pokemons} />
+                <FavoriteLayout pokemons={favoritePokemons} isLoading={isLoading} />
               </div>
             </Choose.When>
             <Choose.When condition={currentTab === menuItems.search}>
@@ -119,7 +80,7 @@ const HomeLayout = () => {
             </Choose.When>
             <Choose.When condition={currentTab === menuItems.all}>
               <div style={{ padding: 16, height: '100vh' }}>
-                <AllLayout
+                <CategoryLayout
                   tags={tagList}
                   cards={filteredAll}
                   onSearch={onSearchByTag}
